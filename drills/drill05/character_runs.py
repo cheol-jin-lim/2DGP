475a7f7104bb6082f2f1b66d49405cab31c4ce19
lim cheol jin(Nitro5) <dlacjfwls104@naver.com>
2018-09-17 17:09:38 +0900

    drill05 첫번째좌표
    첫번째 좌표에 소년이 나오도록 하였고 다음 가는방향이 왼쪽 방향으로이동 하므로 왼쪽을 바라보게 하였다.

--- drills/drill05/character_runs.py
+++ drills/drill05/character_runs.py
@@ -3,8 +3,17 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-x = 0
 def one_position():
+
+    x,y = 203,535
+    clear_canvas()
+    grass.draw(400, 30)
+    character.clip.draw(0,300,100,100,203,535)
+
+    update_canvas()
+    get_events()
+
+
     pass
 def two_position():
     pass
@@ -26,16 +35,17 @@ def ten_position():
     pass
 
 def make_running():
-    one_position()
-    two_position()
-    three_position()
-    four_position()
-    five_position()
-    six_position()
-    seven_position()
-    eight_position()
-    nine_position()
-    ten_position()
+    while True:
+        one_position()
+        two_position()
+        three_position()
+        four_position()
+        five_position()
+        six_position()
+        seven_position()
+        eight_position()
+        nine_position()
+        ten_position()
     pass
 
 
