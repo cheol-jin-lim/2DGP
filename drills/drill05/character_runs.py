183a5e12cf511daefa07d05ffef0072e5223dbf4
lim cheol jin(Nitro5) <dlacjfwls104@naver.com>
2018-09-17 18:06:45 +0900

    drill05 두번째좌표
    두번째 좌표인 (132,243)으로 이동하도록 만들었음.

--- drills/drill05/character_runs.py
+++ drills/drill05/character_runs.py
@@ -1,21 +1,35 @@
 from pico2d import *
-open_canvas()
+open_canvas(800,600)
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
+frame = 0
+
+
 
 def one_position():
-
     x,y = 203,535
     clear_canvas()
     grass.draw(400, 30)
-    character.clip.draw(0,300,100,100,203,535)
-
+    character.clip_draw(frame,200,100,100,x,y)
     update_canvas()
     get_events()
+pass
 
-
-    pass
 def two_position():
+    x, y = 203, 535
+    while x > 132 and y > 243:
+        frame = 0
+        clear_canvas()
+        grass.draw(400,30)
+        character.clip_draw(frame*100,0,100,100,x,y)
+
+        update_canvas()
+        frame = (frame + 1) % 8
+        x = x - 2
+        y = y - 10 # x축이 이동하는 거리보다 y축이 이동하는 거리를 크게해줌.
+        delay(0.05)
+        get_events()
+
     pass
 def three_position():
     pass
@@ -34,19 +48,19 @@ def nine_position():
 def ten_position():
     pass
 
-def make_running():
-    while True:
-        one_position()
-        two_position()
-        three_position()
-        four_position()
-        five_position()
-        six_position()
-        seven_position()
-        eight_position()
-        nine_position()
-        ten_position()
-    pass
+
+# while True:
+    one_position()
+    two_position()
+    three_position()
+    four_position()
+    five_position()
+    six_position()
+    seven_position()
+    eight_position()
+    nine_position()
+    ten_position()
+
 
 
 
