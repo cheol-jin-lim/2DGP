a4e129c3d1bfaa8267ace64e579d93f065394ec7
lim cheol jin(Nitro5) <dlacjfwls104@naver.com>
2018-09-17 18:41:17 +0900

    drill05 다섯번째 좌표로 이동
    다섯 번째 좌표로 이동하는 것을 만들었고 x와y값의 증가 및 감소량을 다르게 해주었다.

--- drills/drill05/character_runs.py
+++ drills/drill05/character_runs.py
@@ -67,6 +67,20 @@ def four_position():
         get_events()
     pass
 def five_position():
+    x,y = 477,203
+    frame = 0
+    while x < 715 and y > 136:
+
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+
+        update_canvas()
+        frame = (frame % 8) + 1
+        x = x + 8
+        y = y - 2
+        delay(0.05)
+        get_events()
     pass
 def six_position():
     pass
@@ -84,7 +98,7 @@ while True:
     #one_position()
     #two_position()
     #three_position()
-    four_position()
+    #four_position()
     five_position()
     six_position()
     seven_position()
