daae7063b7341efe526ab4b483e0a8470629d2f3
lim cheol jin(Nitro5) <dlacjfwls104@naver.com>
2018-09-17 16:24:36 +0900

    drill05 왼쪽으로 이동
    move_up을 실행한뒤 왼쪽으로 이동하도록 만듬.

--- drills/drill05(특강)/drill05.py
+++ drills/drill05(특강)/drill05.py
@@ -25,6 +25,13 @@ def move_up():
         delay(0.01)
     pass
 def move_left():
+    x, y = 800 - 25, 600 - 50
+    while x > 0 + 25:
+        clear_canvas_now()
+        grass.draw(400, 30)
+        character.draw_now(x, y)
+        x = x - 2
+        delay(0.01)
     pass
 def move_down():
     pass
@@ -36,7 +43,7 @@ def move_from_left_to_center():
 
 def make_rectangle():
     # move_from_center_to_right()
-    move_up()
+    # move_up()
     move_left()
     move_down()
     move_from_left_to_center()
