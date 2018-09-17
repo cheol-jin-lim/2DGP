e3d3e426b844530c1e93b594315bf2bb6c82832c
lim cheol jin(Nitro5) <dlacjfwls104@naver.com>
2018-09-17 17:01:54 +0900

    drill05 뼈대 만들기
    함수의 뼈대를 만들었습니다.

---
+++ drills/drill05/character_runs.py
@@ -0,0 +1,47 @@
+from pico2d import *
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+x = 0
+def one_position():
+    pass
+def two_position():
+    pass
+def three_position():
+    pass
+def four_position():
+    pass
+def five_position():
+    pass
+def six_position():
+    pass
+def seven_position():
+    pass
+def eight_position():
+    pass
+def nine_position():
+    pass
+def ten_position():
+    pass
+
+def make_running():
+    one_position()
+    two_position()
+    three_position()
+    four_position()
+    five_position()
+    six_position()
+    seven_position()
+    eight_position()
+    nine_position()
+    ten_position()
+    pass
+
+
+
+
+
+
+close_canvas()
+
