40592e8aa7dc682080cf96b10729a31a33206a6b
lim cheol jin(Nitro5) <dlacjfwls104@naver.com>
2018-11-03 20:57:59 +0900

    project
    blue_enemy에 시간을 적용하여 자연스럽게 움직이도록 하였다.

--- 2D game project/project/blue_enemy.py
+++ 2D game project/project/blue_enemy.py
@@ -2,13 +2,22 @@ from pico2d import *
 
 
 import game_world
-
+import game_framework
 
 
 
 
 
 class Blue_enemy:
+    PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+    RUN_SPEED_KMPH = 20.0  # Km / Hour
+    RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+    RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+    RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+    TIME_PER_ACTION = 1
+    ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+    FRAMES_PER_ACTION = 1
 
 
     def __init__(self, i):
@@ -16,13 +25,17 @@ class Blue_enemy:
         self.x = 50+50 * i
         self.y = 450
         self.frame = 0
+        self.total_frame = 0.0
 
 
 
 
 
     def update(self):
-        self.frame = (self.frame + 1) % 2
+        self.total_frame += Blue_enemy.FRAMES_PER_ACTION * Blue_enemy.ACTION_PER_TIME * game_framework.frame_time
+        # self.frame = (self.frame + 1) % 2
+        self.frame = int(self.total_frame) % 2
+
         pass
 
 
--- 2D game project/project/blue_enemy2.py
+++ 2D game project/project/blue_enemy2.py
@@ -2,6 +2,7 @@ from pico2d import *
 
 
 import game_world
+import game_framework
 
 
 
@@ -9,6 +10,15 @@ import game_world
 
 
 class Blue_enemy2:
+    PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+    RUN_SPEED_KMPH = 20.0  # Km / Hour
+    RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+    RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+    RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+    TIME_PER_ACTION = 1
+    ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+    FRAMES_PER_ACTION = 1
 
 
     def __init__(self, i):
@@ -16,13 +26,14 @@ class Blue_enemy2:
         self.x = 50+50 * i
         self.y = 400
         self.frame = 0
+        self.total_frame = 0.0
 
 
 
 
 
     def update(self):
-        self.frame = (self.frame + 1) % 2
+
         pass
 
 
